7b04fb77f950ad50255df486360c3ff54e3e0444
Ameliawangari <amelia.gitau@gmail.com>
2023-03-22 09:33:29 -0700

    ./5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: is the character to be checked
+ * Return: 1, 0 and -1
+ */
+
+int print_sign(int n)
+
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}

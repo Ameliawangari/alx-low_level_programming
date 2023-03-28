e4cb6fd5f76e6fe32e694cc4b80d52f3c4347d1c
Ameliawangari <amelia.gitau@gmail.com>
2023-03-27 17:04:51 -0700

    resett to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that takes as a parameter
+ * updates the value to 98
+ * @n: integer to be checked
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
